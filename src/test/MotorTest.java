7e40862f8f92a3775cd7bbe80562c6a17bcd00ee
Lirppis <60780018+Lirppis@users.noreply.github.com>
2025-04-25 14:13:58 +0300

    Update MotorTest.java
    Added threaded code

--- src/test/MotorTest.java
+++ src/test/MotorTest.java
@@ -1,58 +1,36 @@
 package test;
 
-import lejos.robotics.RegulatedMotor; // Port D: Left motor | Port C: Right motor
 import lejos.hardware.motor.Motor;
-import lejos.hardware.motor.NXTRegulatedMotor;
-import lejos.hardware.lcd.LCD;
-import lejos.utility.Delay;
-import lejos.hardware.sensor.EV3UltrasonicSensor; // Port 4
-import lejos.hardware.port.MotorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.port.SensorPort;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.Button;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class MotorTest extends Thread{
-    EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
-    // Create a list to ensure that the motors are running in sync
-    RegulatedMotor[] syncList = {Motor.C};
-    SonicSensor sonicS = new SonicSensor();
+    int baseMotorSpeed = 200;
+    int leftMotorSpeed;
+    int rightMotorSpeed;
     
-    // Stopping motors
-    public void stopMotors() {
-        //Syncing the motors and moving forward
-        Motor.D.synchronizeWith(syncList);
-        Motor.D.startSynchronization();
-        // Stopping both motors without braking
-        Motor.D.flt();
-        Motor.C.flt();
-        Motor.D.endSynchronization();
-        // Ending the sync, needs to be done everytime stopping or changing motor behaviour  
-        ultrasonicSensor.disable();
+    public void Forward(){
+    Motor.C.setSpeed(baseMotorSpeed);
+    Motor.D.setSpeed(baseMotorSpeed);
+    Motor.C.forward();
+    Motor.D.forward();
     }
 
-    // Strating motors
-    public void startMotors() {
-        ultrasonicSensor.enable();
-        Motor.D.synchronizeWith(syncList);
-        Motor.D.startSynchronization();
-        Motor.D.forward();                          
-        Motor.C.forward();                        
-        Motor.D.endSynchronization();
+    public void SpeedAdjustRight(int rightMotorSpeed){
+        Motor.C.setSpeed(Math.max(0, rightMotorSpeed));
     }
 
-    // Turning
-    public void turnMotors(){
-        Motor.D.synchronizeWith(syncList);
-        //Rotating both motors 410 degrees to opposite directions
-        Motor.D.startSynchronization();
-        Motor.D.rotate(410);
-        Motor.C.rotate(-410);
-        Motor.D.endSynchronization();
-        // Waiting for the rotation to finnish 
-        while (Motor.D.isMoving()){
-            Thread.yield();
-        }
+    public void SpeedAdjustLeft(int leftMotorSpeed){
+        Motor.D.setSpeed(Math.max(0, leftMotorSpeed));
+    }
+
+    public void StopMotors(){
+        Motor.D.stop(true);
+        Motor.C.stop(true);
     }
 
 }
-
-
-
