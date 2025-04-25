6cf6508059562fc1b75284aefdb2ccafe3f65032
snak1o <64849331+snak1o@users.noreply.github.com>
2025-04-25 13:38:35 +0300

    Add comments for better code understading

--- src/test/LineFollower.java
+++ src/test/LineFollower.java
@@ -11,6 +11,7 @@ import lejos.utility.Delay;
 public class LineFollower {
 
     public static void main(String[] args) {
+        // init color sensor
         EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
 
         SampleProvider redModeSensorProvider = colorSensor.getRedMode();
@@ -24,18 +25,22 @@ public class LineFollower {
         Motor.C.forward();
         Motor.D.forward();
 
-        float proportionalGain = 600; // Kp
-        float integralGain = 0.02f;  // Ki
-        float derivativeGain = 700;  // Kd
+        // PID constants    
+        float proportionalGain = 600; // Kp: current error reaction
+        float integralGain = 0.02f;  // Ki: history of errors
+        float derivativeGain = 700;  // Kd: future error reaction
 
-
-        float integralAccumulator = 0;
+        // history of errors
+        float integralError = 0;
+        // previous value of red light sensor
         float previousError = 0;
         
+        // min and max threshold for smooth moves
         float minThreshold = 0.17f;
         float maxThreshold = 0.23f;
 
         while (!Button.ESCAPE.isDown()) {
+            // get red light value 
             redModeSensorProvider.fetchSample(sensorSampleArray, 0);
             float currentSensorReading = sensorSampleArray[0];
 
@@ -45,20 +50,27 @@ public class LineFollower {
             float currentError = 0;
             float steeringAdjustment;
 
+            // while line is between min and max threshold it doesnt change speed and direction
             if (currentSensorReading >= minThreshold && currentSensorReading <= maxThreshold) {
                 steeringAdjustment = 0;
-                integralAccumulator = 0;
+                integralError = 0;
             } else {
+                // if red light value is too small it turns left
                 if (currentSensorReading < minThreshold) {
                     currentError = currentSensorReading - minThreshold;
                 } else {
+                    // if red light value is too big it turns right
                     currentError = currentSensorReading - maxThreshold;
                 }
 
-                integralAccumulator += currentError;
+                // history of errors
+                integralError += currentError;
+                // future error reaction
                 float derivativeOfError = currentError - previousError;
+
+                // PID formula
                 steeringAdjustment = (proportionalGain * currentError) +
-                                       (integralGain * integralAccumulator) +
+                                       (integralGain * integralError) +
                                        (derivativeGain * derivativeOfError);
             }
 
@@ -67,6 +79,7 @@ public class LineFollower {
             int leftMotorSpeed = (int)(baseMotorSpeed + steeringAdjustment);
             int rightMotorSpeed = (int)(baseMotorSpeed - steeringAdjustment);
 
+            // Math.max is for preventing negative speed
             Motor.C.setSpeed(Math.max(0, leftMotorSpeed));
             Motor.D.setSpeed(Math.max(0, rightMotorSpeed));
 
