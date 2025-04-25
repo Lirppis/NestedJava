1c88099922480c523a2e0a083a9148b16ddd0fb6
Lirppis <60780018+Lirppis@users.noreply.github.com>
2025-04-25 14:12:54 +0300

    Update LineFollower.java
    Added the threaded code

--- src/test/LineFollower.java
+++ src/test/LineFollower.java
@@ -1,3 +1,4 @@
+
 package test;
 
 import lejos.hardware.motor.Motor;
@@ -8,78 +9,70 @@ import lejos.hardware.Button;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-public class LineFollower {
+public class LineFollower extends Thread{
 
-    public static void main(String[] args) {
-        // Create and configure the color sensor
+    public void run() {
+        MotorTest mControl = new MotorTest();
         EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
 
-        // You can try & use other modes to see the difference & feel free to use the mode that
-        // suites your needs the best, for instance, getAmbientMode(), getRedMode(), etc
-        // Here we are setting the sensor to red mode (measures reflected red light)
-        SampleProvider light = colorSensor.getRedMode();  // Use red mode for reflected light intensity
+        SampleProvider redModeSensorProvider = colorSensor.getRedMode();
 
-        // Create an array to hold the sensor data
-        float[] sample = new float[light.sampleSize()];
+        float[] sensorSampleArray = new float[redModeSensorProvider.sampleSize()];
 
-        // Set motor speeds
-        Motor.A.setSpeed(300);
-        Motor.B.setSpeed(300);
+        mControl.Forward();
 
-        // Start motors moving forward
-        Motor.A.forward();
-        Motor.B.forward();
+        float proportionalGain = 600; // Kp
+        float integralGain = 0.02f;  // Ki
+        float derivativeGain = 700;  // Kd
+        float integralAccumulator = 0;
+        float previousError = 0;
+        float minThreshold = 0.17f;
+        float maxThreshold = 0.23f;
+        int baseMotorSpeed = 200;
 
-        // Continuously follow the line until a button is pressed
         while (!Button.ESCAPE.isDown()) {
-            // Get the current red light intensity reading from the sensor
-            light.fetchSample(sample, 0);  // 0 is the index where data will be stored
-            
-            // for debugging purposes, better to display light intensity on LCD
+            redModeSensorProvider.fetchSample(sensorSampleArray, 0);
+            float currentSensorReading = sensorSampleArray[0];
+
             LCD.clear();
-            LCD.drawString("Red Light Intensity: " + (int)(sample[0] * 100) + "%", 0, 0);
+            LCD.drawString("Intensity: " + (int)(currentSensorReading * 100), 0, 0);
+
+            float currentError = 0;
+            float steeringAdjustment;
+
+            if (currentSensorReading >= minThreshold && currentSensorReading <= maxThreshold) {
+                steeringAdjustment = 0;
+                integralAccumulator = 0;
+            } else {
+                if (currentSensorReading < minThreshold) {
+                    currentError = currentSensorReading - minThreshold;
+                } else {
+                    currentError = currentSensorReading - maxThreshold;
+                }
+
+                integralAccumulator += currentError;
+                float derivativeOfError = currentError - previousError;
+                steeringAdjustment = (proportionalGain * currentError) +
+                                       (integralGain * integralAccumulator) +
+                                       (derivativeGain * derivativeOfError);
+            }
+
+            previousError = currentError;
+
+            int rightMotorSpeed = (int)(baseMotorSpeed + steeringAdjustment);
+            int leftMotorSpeed = (int)(baseMotorSpeed + steeringAdjustment);
+
+            mControl.SpeedAdjustRight(rightMotorSpeed);
+            mControl.SpeedAdjustLeft(leftMotorSpeed);
+
+            mControl.Forward();
             
-            // Threshold for detecting the black line
-            // NOTE: You'll most probably have to fine tune the threshold value
-            float threshold = 0.2f;  // Adjusted threshold value for the black line detection
-
-            // If the light intensity is low (black line), the robot is on the line
-            if (sample[0] < threshold)
-            { 
-                Motor.A.setSpeed(300);
-                Motor.B.setSpeed(300);
-                Motor.A.forward();
-                Motor.B.forward();
-            }
-            else                        // Off the black line, adjust to turn towards the line
-            {
-                // Work on these logics. they are just for illustration purposes.
-                // The turns defined below can be inverted turns. So test yourselves and rectify accordingly
-                if (sample[0] > 0.6)
-                {
-                    // If it's very bright (white surface), turn left
-                    Motor.A.setSpeed(300);
-                    Motor.B.setSpeed(150);
-                }
-                else
-                {
-                    // If it's somewhat bright (near the edge of the line), turn right
-                    Motor.A.setSpeed(150);
-                    Motor.B.setSpeed(300);
-                }
-                Motor.A.forward();
-                Motor.B.forward();
-            }
-
-            // Add a small delay to reduce the frequency of updates
-            Delay.msDelay(50);
+            Delay.msDelay(10);
         }
 
-        // Stop the motors before exiting
-        Motor.A.stop();
-        Motor.B.stop();
-        
-        // Remember to close the sensor before exiting
+        Motor.C.stop(true);
+        Motor.D.stop(true);
+
         colorSensor.close();
     }
 }
